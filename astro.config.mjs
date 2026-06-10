53e593a72580e8eff7e40948a5b1c60fd6b2f98f
Yuvraj Verma <106587558+yashv-77@users.noreply.github.com>
2026-06-11 02:10:39 +0530

    feat: implement seperate tool pages for seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://devjsonsandbox.pages.dev",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 })
